cb848a255d8fdde61549bd8b0d55c26adf1d8286
mikealley <bali_waters@yahoo.com>
2018-07-21 15:54:07 -0300

    inclusao do gitignore bower

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>NPSL Teams</title>
+  <link rel="stylesheet" type="text/css" href="css1.css"/>
+  </head>
+  <body>
+  <h1 class="h1"> Salve </h1>
+  
+  </body>
+  
+  </html>
